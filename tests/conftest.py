a43b4c42b43ea71c654c5868197c15bb92f8febc
nikita <tele2.rumerkulov@yandex.ru>
2024-04-18 10:40:47 +0500

    Sprint_6

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from pages.order_page import OrderPage
 from pages.yandex_samokat_page import SamokatPage
 
+
 @pytest.fixture
 def browser():
     driver_browser = webdriver.Chrome()
@@ -16,12 +17,14 @@ def browser():
     yield driver_browser
     driver_browser.quit()
 
+
 @pytest.fixture
 def samokat(browser):
     samokat_page = SamokatPage(browser)
     samokat_page.open()
     samokat_page.button_cookie()
     return samokat_page
+
 
 @pytest.fixture
 def order_page(browser):
