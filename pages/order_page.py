7143634b8f77248a48fe8b68b81a5bc4c58d0f15
nikita <tele2.rumerkulov@yandex.ru>
2024-04-18 10:22:35 +0500

    Sprint_6

--- pages/order_page.py
+++ pages/order_page.py
@@ -1,29 +1,12 @@
-import time
-
-from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
 from tests.data import name, lastname, address, telephone, delivery
 from tests.urls import URL
+from locators.order_page_locators import cookie, button_order, name_order, lastname_order, address_order, \
+    metro_station_order, station, telephone_order, next_button_order, delivery_order, close_calendar_order, \
+    period_order, day_order, color_order, button, button_yes_order, order_is_processed
 
 
 class OrderPage(BasePage):
-    cookie = (By.ID, 'rcc-confirm-button')
-    button_order = (By.CLASS_NAME, 'Button_Button__ra12g')
-    name_order = (By.CSS_SELECTOR, '[placeholder="* Имя"]')
-    lastname_order = (By.CSS_SELECTOR, '[placeholder="* Фамилия"]')
-    address_order = (By.CSS_SELECTOR, '[placeholder="* Адрес: куда привезти заказ"]')
-    metro_station_order = (By.CSS_SELECTOR, '[placeholder="* Станция метро"]')
-    station = (By.CSS_SELECTOR, 'div.Order_Text__2broi')
-    telephone_order = (By.CSS_SELECTOR, '[placeholder="* Телефон: на него позвонит курьер"]')
-    next_button_order = (By.XPATH, "//button[text()='Далее']")
-    delivery_order = (By.CSS_SELECTOR, '[placeholder="* Когда привезти самокат"]')
-    period_order = (By.CSS_SELECTOR, '.Dropdown-placeholder')
-    day_order = (By.XPATH, "//div[@class='Dropdown-option'and text()='сутки']")
-    color_order = (By.ID, 'black')
-    button = (By.XPATH, "//button[@class='Button_Button__ra12g Button_Middle__1CSJM']")
-    close_calendar_order = (By.XPATH, "//div[@class='App_App__15LM-']")
-    button_yes_order = (By.XPATH, "//button[text()='Да']")
-    order_is_processed = (By.XPATH, "//div[@class='Order_Text__2broi']")
 
     def __init__(self, browser):
         super().__init__(browser)
@@ -34,72 +17,72 @@ class OrderPage(BasePage):
 
     # Принимаем куки
     def button_cookie(self):
-        self.browser.find_element(*self.cookie).click()
+        self.browser.find_element(*cookie).click()
 
     # Нажимаем кнопку "Заказать" в шапке
     def order_button(self):
-        self.browser.find_element(*self.button_order).click()
+        self.browser.find_element(*button_order).click()
 
     # Вводим имя
     def name_order_send(self, ):
-        self.browser.find_element(*self.name_order).send_keys(name)
+        self.browser.find_element(*name_order).send_keys(name)
 
     # Вводим фамилию
     def lastname_order_send(self, ):
-        self.browser.find_element(*self.lastname_order).send_keys(lastname)
+        self.browser.find_element(*lastname_order).send_keys(lastname)
 
     # Вводим адрес
     def address_order_send(self, ):
-        self.browser.find_element(*self.address_order).send_keys(address)
+        self.browser.find_element(*address_order).send_keys(address)
 
     # Нажимаем на поле "Станция метро"
     def metro_station_click(self, ):
-        self.browser.find_element(*self.metro_station_order).click()
+        self.browser.find_element(*metro_station_order).click()
 
     # Выбираем станцию "Бульвар Рокоссовского"
     def station_click(self, ):
-        self.browser.find_element(*self.station).click()
+        self.browser.find_element(*station).click()
 
     # Вводим номер телефона
     def telephone_order_send(self, ):
-        self.browser.find_element(*self.telephone_order).send_keys(telephone)
+        self.browser.find_element(*telephone_order).send_keys(telephone)
 
     # Метод нажимает кнопку "Далее"
     def next_button_order_click(self):
-        self.browser.find_element(*self.next_button_order).click()
+        self.browser.find_element(*next_button_order).click()
 
     # Метод указывает дату когда привезти самокат
     def delivery_order_send(self):
-        self.browser.find_element(*self.delivery_order).send_keys(delivery)
+        self.browser.find_element(*delivery_order).send_keys(delivery)
 
     # Метод закрывает календарь
     def close_calendar_order_click(self):
-        self.browser.find_element(*self.close_calendar_order).click()
+        self.browser.find_element(*close_calendar_order).click()
 
     # Метод нажимает на поле "Срок аренды"
     def period_order_click(self):
-        self.browser.find_element(*self.period_order).click()
+        self.browser.find_element(*period_order).click()
 
     # Метод выбирает срок аренды "Сутки"
     def day_order_click(self):
-        self.browser.find_element(*self.day_order).click()
+        self.browser.find_element(*day_order).click()
 
     # Метот выбирает цвет самоката
     def color_order_click(self):
-        self.browser.find_element(*self.color_order).click()
+        self.browser.find_element(*color_order).click()
 
     # Метод нажимает кнопку "Заказать" под формой
     def button_click(self):
-        self.browser.find_element(*self.button).click()
+        self.browser.find_element(*button).click()
 
     # Метод нажимает "Да" в окне оформления заказ
     def button_yes_order_click(self):
-        self.browser.find_element(*self.button_yes_order).click()
+        self.browser.find_element(*button_yes_order).click()
 
     # Метод возвращает "Заказ успешно оформлен"
 
     def return_order_is_processed(self):
-        return self.browser.find_element(*self.order_is_processed)
+        return self.browser.find_element(*order_is_processed)
 
     # Метод объединяет остальные в шаг
     def order(self):
