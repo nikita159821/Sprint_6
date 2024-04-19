aaf94213aa32b89c5c2e1ebdde7d17a42dbba5a0
nikita159821 <132481753+nikita159821@users.noreply.github.com>
2024-04-19 12:28:46 +0500

    Update test_logo_redirection.py

--- tests/test_logo_redirection.py
+++ tests/test_logo_redirection.py
@@ -1,13 +1,17 @@
+import allure
 from tests.urls import URL, URL_DZEN
 
 
 class TestLogoRedirection:
+
+    @allure.title('Проверка редиректа на домашнюю страницу')
     def test_logo_redirects_to_home(self, samokat, browser):
         samokat.logo_click()
-        assert browser.current_url == URL
+        assert samokat.get_current_url() == URL
 
+    @allure.title('Проверка редиректа на страницу Яндекс дзен')
     def test_yandex_logo_redirects_to_zen(self, samokat, browser):
         samokat.logo_yandex_click()
         all_windows = browser.window_handles
         browser.switch_to.window(all_windows[1])
-        assert browser.current_url == URL_DZEN
+        assert samokat.get_current_url() == URL_DZEN
