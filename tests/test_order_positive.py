3aed49e7c8767ff7625738304a52b592fcaf22ee
nikita159821 <132481753+nikita159821@users.noreply.github.com>
2024-04-19 12:28:56 +0500

    Update test_order_positive.py

--- tests/test_order_positive.py
+++ tests/test_order_positive.py
@@ -1,15 +1,18 @@
+import allure
 import pytest
 from tests.data import order_data_top_button, order_data_bottom_button
 
 
 class TestOrder:
 
+    @allure.title('Проверка оформления заказа через кнопку в шапке сайта')
     @pytest.mark.parametrize('name, lastname, address, telephone, delivery', order_data_top_button)
     def test_order_positive_top_button(self, order_page, name, lastname, address, telephone, delivery):
         order_page.order_button_click()
         order_page.order(name, lastname, address, telephone, delivery)
         assert order_page.return_order_is_processed().is_displayed()
 
+    @allure.title('Проверка оформления заказа через кнопку в нижней части страницы')
     @pytest.mark.parametrize('name, lastname, address, telephone, delivery', order_data_bottom_button)
     def test_order_scooter_from_bottom_button(self, order_page, name, lastname, address, telephone, delivery):
         order_page.order_bottom_button_click()
